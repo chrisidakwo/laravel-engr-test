9ea38d2ffa52d892f302e735198747c2efda9c4d
Ekojoka Chris Idakwo <chris.idakwo@gmail.com>
2024-09-02 22:47:33 +0100

    Added date casting for Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,13 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
     use HasFactory;
 
-    protected $guarded = [];
+    protected $casts = [
+        'encounter_date' => 'date',
+    ];
+
+    public function hmo(): BelongsTo
+    {
+        return $this->belongsTo(Hmo::class);
+    }
 
     public function orderItems(): HasMany
     {
